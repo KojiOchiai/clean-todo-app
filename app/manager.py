7c34077656a054c43793f82cee8c258d2e9409b8
KojiOchiai <12869331+KojiOchiai@users.noreply.github.com>
2024-12-29 02:15:19 +0900

    feat: enhance task management by associating tasks with user_id
    - Updated set_task_status, delete_task, and update_task methods in TaskManager to include user_id parameter for improved task ownership validation.
    - Added user_id checks to ensure that only the owner can modify or delete their tasks, enhancing error handling and data integrity.

--- app/manager.py
+++ app/manager.py
@@ -23,28 +23,30 @@ class TaskManager:
     def get_tasks_by_user_id(self, user_id: int) -> list[Todo]:
         return self.storage.get_tasks_by_user_id(user_id)
 
-    def set_task_status(self, task_id: int, is_done: bool) -> None:
+    def set_task_status(self, user_id: int, task_id: int, is_done: bool) -> None:
         task = self.storage.get_task(task_id)
-        if task is None:
+        if task is None or task.user_id != user_id:
             raise ValueError("Task not found")
         self.storage.update_status(task_id, is_done)
 
-    def delete_task(self, task_id: int) -> None:
+    def delete_task(self, user_id: int, task_id: int) -> None:
+        task = self.storage.get_task(task_id)
+        if task is None or task.user_id != user_id:
+            raise ValueError("Task not found")
         self.storage.delete(task_id)
 
     def update_task(
-        self, task_id: int, title: str = None, description: str = None
+        self, user_id: int, task_id: int, title: str = None, description: str = None
     ) -> Todo:
         task = self.storage.get_task(task_id)
-        if task:
-            if title is not None:
-                task.title = title
-            if description is not None:
-                task.description = description
-            self.storage.update(task)
-            return task
-        else:
+        if task is None or task.user_id != user_id:
             raise ValueError("Task not found")
+        if title is not None:
+            task.title = title
+        if description is not None:
+            task.description = description
+        self.storage.update(task)
+        return task
 
 
 class UserManager:
